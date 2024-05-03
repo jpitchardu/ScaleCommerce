a9260657c916f32962f17a2c8bea5704035cf087
Jonathan Pichardo <jonathan.pichardo@classdojo.com>
2024-05-03 17:56:06 +0000

    Make scripts

---
+++ scripts/make/dev.mk
@@ -0,0 +1,16 @@
+.PHONY: status logs start stop clean
+
+status: ## Show status of docker containers
+	docker-compose ps
+
+logs: ## Show logs of docker containers
+	docker-compose logs -f
+
+start: ## Start docker containers
+	docker-compose up -d
+
+stop: ## Stop docker containers
+	docker-compose stop
+
+clean:stop ## Stop docker containers, clean data and workspace
+	docker-compose down -v --remove-orphans
